5c25cd9e9b7e1fc058e7bb193e17d9df4bc2e2b2
Caden Wilcox <wilcox4357@gmail.com>
2021-05-13 21:01:26 -0700

    fav books

---
+++ urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('register', views.register),
+    path('login', views.login),
+    path('blog', views.blog_page),
+    path('logout', views.logout),
+    path('create_book', views.create_book),
+    path("unfavorite/<int:id>", views.unfavorite),
+    path("favorite/<int:id>", views.favorite),
+    path("display/<int:id>", views.display)
+
+]
---
+++ views.py
@@ -0,0 +1,88 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import *
+
+def index(request):
+    return render(request, 'index.html')
+
+def register(request):
+    if request.method == "GET":
+        return redirect('/')
+    errors = User.objects.basic_validator(request.POST)
+    if errors:
+        for key,value in errors.items():
+            messages.error(request, value)
+        return redirect('/')
+    else:
+        user = User.objects.register(request.POST)
+        request.session['user_id'] = user.id
+        return redirect('/blog')
+
+
+def login(request):
+    if request.method == "GET":
+        return redirect('/')
+    if not User.objects.authenticate(request.POST['email_address'], request.POST['password']):
+        messages.error(request, "Invalid Email or Password")
+        return redirect('/')
+    user = User.objects.get(email_address=request.POST['email_address'])
+    request.session['user_id'] = user.id
+    return redirect('/blog')
+
+def blog_page(request):
+    if 'user_id' not in request.session:
+        return redirect('/')
+    user = User.objects.get(id=request.session['user_id'])
+    context = {
+        'user': user,
+        'books': Book.objects.all().exclude(users_who_like=user),
+        'users_books': Book.objects.filter(users_who_like=user)
+        }
+    return render(request, 'books.html', context)
+
+def create_book(request):
+    if request.method == "GET":
+        return redirect('/')
+    errors = Book.objects.book_validator(request.POST)
+    if errors:
+        for key,value in errors.items():
+            messages.error(request, value)
+        return redirect('/blog')
+    else:
+        user = User.objects.get(id=request.session['user_id'])
+        book = Book.objects.create(
+            title = request.POST['title'],
+            desc = request.POST['desc'],
+            uploaded_by = user,
+        )
+    user = User.objects.get(id = request.session['user_id'])
+    book=book
+    book.users_who_like.add(user)
+    return redirect('/blog')
+
+def display(request, id):
+    user = User.objects.get(id=request.session['user_id'])
+    context = {
+        'user': user,
+        'this_book': Book.objects.get(id=id)
+    }
+    return render(request, 'display.html', context)
+
+def favorite(request, id):
+    book = Book.objects.get(id=id)
+    user = User.objects.get(id=request.session['user_id'])
+    user.liked_books.add(book)
+    user.save()
+    return redirect('/blog')
+
+def unfavorite(request, id):
+    book = Book.objects.get(id=id)
+    user = User.objects.get(id=request.session['user_id'])
+    book.users_who_like.remove(user)
+    book.save()
+    return redirect('/blog')
+
+
+def logout(request):
+    request.session.clear()
+    return redirect('/')
